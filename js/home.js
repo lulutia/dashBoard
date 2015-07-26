e1dad8544d7475e75ef0d5dada4b8f054a910a5f
lulutia <nowillnowill@gmail.com>
2015-07-26 21:46:15 +0800

    update:timeout

--- js/home.js
+++ js/home.js
@@ -4,4 +4,4 @@
  * Version: 0.0.1
  * Copyright 2015 - 2015 
  */
- require.config({baseUrl:"./",waitSeconds:200,paths:{jquery:"vendor/jquery/dist/jquery",echarts:"vendor/echarts/build/dist/echarts-all"},shim:{echarts:{exports:"echarts"}}});
+ require.config({baseUrl:"./",waitSeconds:0,paths:{jquery:"vendor/jquery/dist/jquery",echarts:"vendor/echarts/build/dist/echarts-all"},shim:{echarts:{exports:"echarts"}}});
