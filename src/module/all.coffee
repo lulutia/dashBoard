39af1afb332dd30e878eb82e416a7c2c7849675e
lulutia <nowillnowill@gmail.com>
2015-07-26 17:16:01 +0800

    update

--- src/module/all.coffee
+++ src/module/all.coffee
@@ -7,7 +7,7 @@ define (require, exports, module) ->
 
     all.init = ->
         all.getData()
-        all.eventBind()
+        
 
     all.getData = ->
         $.ajax {
@@ -25,7 +25,102 @@ define (require, exports, module) ->
             $(".J-info").addClass("hidden")
             value = $("#J-allnav li.active").attr("value")
             $("#" + value).removeClass("hidden").addClass("block")
+            all.airbarchart.clear()
+            all.itemall = []
+            all.itemallname = []
+            all.itemradio = []
             all.dataProcess(all.allType)
+        @airchart.on "click", (param) ->
+            all.airbarinit(param)
+
+        
+
+    all.airbarinit = (param) ->
+        if param?
+            index = all.allType.area.indexOf(param.name)
+        else index = 309
+        all.itemall = all.itemall || []
+        all.itemradio = all.itemradio || []
+        if all.itemall.length < 5
+            all.itemallname = all.itemallname || []
+            all.itemallname.push all.allType.area[index]
+            item = {
+                name: all.allType.area[index]
+                type: 'bar'
+                data: [all.allType.aqi[index],all.allType.pm2_5[index],all.allType.pm10[index],all.allType.co[index],all.allType.no2[index],all.allType.o3[index],all.allType.so2[index]]
+            }
+            all.itemall.push item
+
+            itemradiodata = {
+                value: [all.allType.aqi[index],all.allType.pm2_5[index],all.allType.pm10[index],all.allType.co[index],all.allType.no2[index],all.allType.o3[index],all.allType.so2[index]]
+                name: all.allType.area[index]
+            }
+            all.itemradio.push itemradiodata
+
+            @airbarchart = gChart.init(document.getElementById('airbar_chart'))
+            option = {
+                title : {
+                    text: '所选地区情况',
+                    subtext: '数据取自PM25.in,此处最多选择五个',
+                },
+                tooltip: {
+                    trigger: 'axis'
+                   },
+                legend: {
+                    data: all.itemallname
+                   },
+                xAxis : [
+                    {
+                    type : 'category',
+                    data : ['aqi','PM2.5','PM10','CO','NO2','O3','SO2']
+                    }
+                   ],
+                yAxis : [
+                   {
+                    type : 'value'
+                   }
+                   ],
+                series: all.itemall
+            }
+            @airbarchart.setOption(option)
+
+            @airradiochart = gChart.init(document.getElementById('airradio_chart'))
+            option1 = {
+                title : {
+                    text: '所选地区情况',
+                    subtext: '数据取自PM25.in,此处最多选择五个',
+                },
+                tooltip : {
+                    trigger: 'axis'
+                },
+                legend: {
+                    orient : 'vertical',
+                    x : 'right',
+                    y : 'bottom',
+                    data: all.itemallname
+                },
+                polar : [
+                   {
+                       indicator : [
+                           { text: '空气质量（AQI）', max: 309},
+                           { text: '细颗粒物（PM2.5）', max: 258},
+                           { text: '可吸入颗粒物（PM10）', max: 267},
+                           { text: '一氧化碳（CO）', max: 3},
+                           { text: '二氧化氮（NO2）', max: 101},
+                           { text: '臭氧（O3）', max: 174},
+                           { text: '二氧化硫（SO2）', max: 169}
+                        ]
+                    }
+                ],
+                calculable : true,
+                series : [{
+                    name: "所选地区情况",
+                    type: 'radar',
+                    data: all.itemradio
+                    }]
+            }
+            @airradiochart.setOption(option1)
+                                
 
     all.dataHandle = (data) ->
         all.allType = {}
@@ -136,6 +231,10 @@ define (require, exports, module) ->
             series: data
         }
         @airchart.setOption(option)
-                            
+        all.airbarinit()
+        all.eventBind()
+        
+
+
 
     module.exports = all
--- src/module/all.js
+++ src/module/all.js
@@ -5,8 +5,7 @@ define(function(require, exports, module) {
   all = {};
   gChart = require("echarts");
   all.init = function() {
-    all.getData();
-    return all.eventBind();
+    return all.getData();
   };
   all.getData = function() {
     return $.ajax({
@@ -19,15 +18,126 @@ define(function(require, exports, module) {
     });
   };
   all.eventBind = function() {
-    return $("#J-allnav li").click(function() {
+    $("#J-allnav li").click(function() {
       var value;
       $(this).parent().find("li").removeClass("active");
       $(this).addClass("active");
       $(".J-info").addClass("hidden");
       value = $("#J-allnav li.active").attr("value");
       $("#" + value).removeClass("hidden").addClass("block");
+      all.airbarchart.clear();
+      all.itemall = [];
+      all.itemallname = [];
+      all.itemradio = [];
       return all.dataProcess(all.allType);
     });
+    return this.airchart.on("click", function(param) {
+      return all.airbarinit(param);
+    });
+  };
+  all.airbarinit = function(param) {
+    var index, item, itemradiodata, option, option1;
+    if (param != null) {
+      index = all.allType.area.indexOf(param.name);
+    } else {
+      index = 309;
+    }
+    all.itemall = all.itemall || [];
+    all.itemradio = all.itemradio || [];
+    if (all.itemall.length < 5) {
+      all.itemallname = all.itemallname || [];
+      all.itemallname.push(all.allType.area[index]);
+      item = {
+        name: all.allType.area[index],
+        type: 'bar',
+        data: [all.allType.aqi[index], all.allType.pm2_5[index], all.allType.pm10[index], all.allType.co[index], all.allType.no2[index], all.allType.o3[index], all.allType.so2[index]]
+      };
+      all.itemall.push(item);
+      itemradiodata = {
+        value: [all.allType.aqi[index], all.allType.pm2_5[index], all.allType.pm10[index], all.allType.co[index], all.allType.no2[index], all.allType.o3[index], all.allType.so2[index]],
+        name: all.allType.area[index]
+      };
+      all.itemradio.push(itemradiodata);
+      this.airbarchart = gChart.init(document.getElementById('airbar_chart'));
+      option = {
+        title: {
+          text: '所选地区情况',
+          subtext: '数据取自PM25.in,此处最多选择五个'
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        legend: {
+          data: all.itemallname
+        },
+        xAxis: [
+          {
+            type: 'category',
+            data: ['aqi', 'PM2.5', 'PM10', 'CO', 'NO2', 'O3', 'SO2']
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ],
+        series: all.itemall
+      };
+      this.airbarchart.setOption(option);
+      this.airradiochart = gChart.init(document.getElementById('airradio_chart'));
+      option1 = {
+        title: {
+          text: '所选地区情况',
+          subtext: '数据取自PM25.in,此处最多选择五个'
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        legend: {
+          orient: 'vertical',
+          x: 'right',
+          y: 'bottom',
+          data: all.itemallname
+        },
+        polar: [
+          {
+            indicator: [
+              {
+                text: '空气质量（AQI）',
+                max: 309
+              }, {
+                text: '细颗粒物（PM2.5）',
+                max: 258
+              }, {
+                text: '可吸入颗粒物（PM10）',
+                max: 267
+              }, {
+                text: '一氧化碳（CO）',
+                max: 3
+              }, {
+                text: '二氧化氮（NO2）',
+                max: 101
+              }, {
+                text: '臭氧（O3）',
+                max: 174
+              }, {
+                text: '二氧化硫（SO2）',
+                max: 169
+              }
+            ]
+          }
+        ],
+        calculable: true,
+        series: [
+          {
+            name: "所选地区情况",
+            type: 'radar',
+            data: all.itemradio
+          }
+        ]
+      };
+      return this.airradiochart.setOption(option1);
+    }
   };
   all.dataHandle = function(data) {
     var i, item, j, key, keys, len, len1;
@@ -154,7 +264,9 @@ define(function(require, exports, module) {
       },
       series: data
     };
-    return this.airchart.setOption(option);
+    this.airchart.setOption(option);
+    all.airbarinit();
+    return all.eventBind();
   };
   return module.exports = all;
 });
